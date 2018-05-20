5712b1ddb490e95777c1d645f8eff6297408d60c
Yuan <c85645@gmail.com>
2018-05-20 12:50:09 +0800

    三種函數寫法,胖箭頭函數寫法

--- 上課記錄/TypeScript/test.js
+++ 上課記錄/TypeScript/test.js
@@ -9,3 +9,22 @@ function add(a, b) {
     return a + b;
 }
 console.log(add(x, y));
+// 第一種寫法
+var myFunc = function (nameFunction) {
+    return ('Hello ' + nameFunction() + '.');
+};
+console.log(myFunc(function () {
+    return 'Adam';
+}));
+// 第二種寫法-函數調用串連起來
+var myFunc2 = function (nameFunction) {
+    return ('Hello ' + nameFunction() + '.');
+};
+var printName2 = function (nameFunction, printFunction) {
+    printFunction(myFunc2(nameFunction));
+};
+printName2(function () { return 'Adam2'; }, console.log);
+// 第三種寫法
+var myFunc3 = function (nameFunction) { return ('Hello ' + nameFunction() + '.'); };
+var printName = function (nameFunction, printFunction) { return printFunction(myFunc3(nameFunction)); };
+printName(function () { return "Adam3"; }, console.log);
--- 上課記錄/TypeScript/test.ts
+++ 上課記錄/TypeScript/test.ts
@@ -1,7 +1,7 @@
 var customer = 'Jeff';
 
 function hello(customer) {
-	console.log("Hello! " + customer);
+  console.log("Hello! " + customer);
 }
 
 hello(customer);
@@ -10,7 +10,28 @@ var x = 1;
 var y = '2';
 
 function add(a, b) {
-	return a + b;
+  return a + b;
 }
-
 console.log(add(x, y));
+
+// 第一種寫法
+let myFunc = function (nameFunction) {
+  return ('Hello ' + nameFunction() + '.');
+}
+console.log(myFunc(function () {
+  return 'Adam';
+}));
+
+// 第二種寫法-函數調用串連起來
+let myFunc2 = function (nameFunction) {
+  return ('Hello ' + nameFunction() + '.');
+}
+let printName2 = function (nameFunction, printFunction) {
+  printFunction(myFunc2(nameFunction));
+}
+printName2(function () { return 'Adam2' }, console.log);
+
+// 第三種寫法-使用箭頭函數
+let myFunc3 = (nameFunction) => ('Hello ' + nameFunction() + '.');
+let printName = (nameFunction, printFunction) => printFunction(myFunc3(nameFunction));
+printName(function () { return "Adam3" }, console.log);
